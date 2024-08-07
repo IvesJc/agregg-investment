225ca3867ec96fb586cf902b2670fc9b9d09e39c
Ives Chiba <ivesjundi@gmail.com>
2024-08-07 10:47:39 -0300

    init :tada: project initialized

---
+++ src/main/java/ives/agregginvestment/AgreggInvestmentApplication.java
@@ -0,0 +1,13 @@
+package ives.agregginvestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgreggInvestmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgreggInvestmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ives/agregginvestment/AgreggInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ives.agregginvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgreggInvestmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
